5188fc866c36b9990509e28d2eca30dc27f4dc30
Muhammad Safwan <muhammadsafwan20051@gmail.com>
2026-06-28 04:05:02 +0500

    store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterSlice  from '../features/redux counter/counterSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterSlice,
+  },
 })
 
 
